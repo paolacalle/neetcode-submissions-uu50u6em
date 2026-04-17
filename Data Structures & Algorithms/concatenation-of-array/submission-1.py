3df4ed5877c11a6b87ba5acca227712aab1ffab6
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-17 10:07:36 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # allocate space for double the space
+        n = len(nums)
+        ans = [0] * n * 2
+
+        for i in range(n):
+            ans[i] = ans[i + n] = nums[i]
+
+        return ans
+
+        
