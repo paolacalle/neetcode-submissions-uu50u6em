baeca5b8a071f55859abbe895fcde937d15bd480
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-17 11:24:06 -0400

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        # method removes the need of sorting 
+        min_length = 201
+        for s in strs:
+            min_length = min(len(s), min_length)
+
+        i = 0
+        while i < min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[0:i]
+            i += 1
+
+        return s[:i]
+
+        
