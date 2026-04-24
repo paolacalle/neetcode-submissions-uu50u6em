042f415e9cb4ab4adf4b77c69408695467472cda
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-24 14:32:00 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    # given an array
+    # - sorted in ascending order 
+    # - roated betwen 1 to n times 
+    # - unique 
+
+    # return the minimum elemnt of the array 
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
+        
