071a631cb5499bb778ca75312e0355ecff0f7383
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-25 18:33:22 -0400

    Add: find-target-in-rotated-sorted-array - submission-23

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-23.py
@@ -0,0 +1,51 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r  = 0, len(nums) - 1
+        while l <= r: 
+            m = l + (r - l) // 2
+
+            print(f"start. l: {l}, m : {m}, r : {r}")
+            if nums[m] == target:
+                return m
+
+            if nums[m] > nums[r]:
+                # this means that there exists a 
+                # lower number 
+                # to the right
+                print(f"1st. l: {l}, m : {m}, r : {r}")
+                if nums[m] > target and nums[l] <= target : 
+                    r = m - 1
+                    print(" - update r")
+                else: 
+                    l = m + 1
+                    print(" - update l")
+            
+            else:
+               # check the target number
+               # to determine which way to go 
+                print(f"2nd. l: {l}, m : {m}, r : {r}")
+                if nums[l] <= target and target <= nums[m]:
+                    r = m - 1
+                elif nums[m] <= target and target <= nums[r]:
+                    l = m + 1
+                elif nums[l] <= target:
+                    r = m - 1
+                elif nums[r] <= target:
+                    l = m + 1
+                else:
+                    if nums[m] < target:
+                        l = m + 1
+                    else: 
+                        r = m - 1
+        return -1 
+
+            
+
+# [4,5,6,7,0,1,2]
+# target = 0 
+
+
+
+
+
+
