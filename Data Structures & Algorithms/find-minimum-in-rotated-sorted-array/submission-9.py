871d198306b0f905b4c756b6a38e9af08e16fe63
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-25 17:40:22 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-9

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-9.py
@@ -0,0 +1,37 @@
+class Solution:
+    # given an array
+    # - sorted in ascending order 
+    # - roated betwen 1 to n times 
+    # - unique 
+
+    # return the minimum elemnt of the array 
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        # step # 1: find the split 
+        while l < r: 
+            m = l + (r - l) // 2
+
+            # if the middle is greater than the right 
+            # then the smallest number must be there
+            if nums[m] > nums[r]:
+                # move the window to the right-side
+                l = 1 + m 
+
+            # if the middle is smaller than the left
+            # than the smallest number must be at the left 
+            else:
+                # move the window to the left-side
+                r = m
+
+        return nums[l]
+
+            
+
+
+
+
+    # [3,4,5,6,1,2]
+    # []
+        
+        
