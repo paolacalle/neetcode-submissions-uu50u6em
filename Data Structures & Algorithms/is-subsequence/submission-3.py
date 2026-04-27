dc347e86a7eeaefa01365103edd700994a5ac46f
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-27 10:22:08 -0400

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        n_s = len(s)
+
+        for r in range(len(t)):
+            if not (i < n_s):
+                return True 
+
+            if t[r] == s[i]:
+                i += 1
+
+        return not (i < n_s)
+
+# time-complexity: O(len(t))
+# Space complexity: O(1)
+        
