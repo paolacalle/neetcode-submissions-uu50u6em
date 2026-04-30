1e14474af3e7973d2fdaf7557d77fbd7110c06d9
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-30 11:13:51 -0400

    Add: reorder-linked-list - submission-4

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-4.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # not needed but good saftey 
+        if not head and not head.next:
+            return
+
+        # use fast and slow pointer method
+        slow, fast = head, head.next
+
+        while fast and fast.next:
+            slow = slow.next 
+            fast = fast.next.next
+
+        # reverse the right half  : O(N / 2)
+        pos = slow.next
+        slow.next = None
+        reverseHead = None
+    
+        while pos:
+           oldPos = pos
+           pos = pos.next 
+           oldPos.next = reverseHead
+           reverseHead = oldPos
+
+        # merge left and reversed right : O(n / 2)
+        addPos = head
+        while reverseHead: 
+            # iterate
+            nextLeft = addPos.next 
+            nextRight = reverseHead.next 
+
+            # update the pos with the reverse
+            addPos.next = reverseHead
+            reverseHead.next = nextLeft
+
+            # update pointers to next points to check
+            addPos = nextLeft
+            reverseHead = nextRight
+            
+# time complexit: O(n) and space complexity : O(1)
+
