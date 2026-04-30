0936f9c494f04bb2fb067ceb2c04589bd47cef91
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-04-30 10:59:56 -0400

    Add: reorder-linked-list - submission-2

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-2.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        # determine length of the list : 0(n)
+        n, pos = 1, head
+        while pos.next:
+            pos = pos.next
+            n += 1
+
+        # determine how many to keep in right order : o(n)
+        left_read = n // 2 if n % 2 == 0 else n // 2 + 1 
+        left_end_pos = head
+        while left_read != 1: 
+            left_end_pos = left_end_pos.next
+            left_read -= 1
+
+        # reverse all after that  : O(N / 2)
+        pos = left_end_pos.next
+        left_end_pos.next = None
+        reverseHead = None
+    
+        while pos:
+           oldPos = pos
+           pos = pos.next 
+           oldPos.next = reverseHead
+           reverseHead = oldPos
+
+        # add in correct pos : O(n / 2)
+        addPos = head
+        while reverseHead: 
+            nextPos = addPos.next 
+            addPos.next = reverseHead
+            reverseHead = reverseHead.next
+            addPos.next.next = nextPos
+            addPos = nextPos
+            
+# time complexit: O(n) and space complexity : O(1)
+
