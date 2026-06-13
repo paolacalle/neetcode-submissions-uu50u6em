bcbee849920b4fad6e2e1cbafed188d66d6f4560
Paola Calle <98432607+paolacalle@users.noreply.github.com>
2026-06-13 14:35:57 -0400

    Add: median-of-two-sorted-arrays - submission-12

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-12.py
@@ -0,0 +1,59 @@
+class Solution:
+
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        a, b = nums1, nums2
+
+        # figure out the array with
+        # greatest length
+        if len(a) > len(b):
+            a, b = b, a 
+
+        # find the position
+        size = len(a) + len(b)
+        mid = size // 2
+
+        # iterate the biggest list
+        l, r = 0, len(a) - 1
+        n, m = len(a), len(b)
+
+        while True:
+            ai = (l + r) // 2 # mid of array a 
+            bi = mid - ai - 2 # mid of arry b 
+
+            # update the in array pointers
+            # fix out of bound
+            a_left = a[ai] if ai >= 0 else float('-inf')
+            b_left = b[bi] if bi >= 0 else float('-inf')
+            a_right = a[ai + 1] if ai + 1 < n else float('inf')
+            b_right = b[bi + 1] if bi + 1 < m else float('inf')
+
+            if a_left <= b_right and b_left <= a_right:
+                # we are a correct partition
+                # that is if we merge them, then the median should be there
+                if size % 2 == 1:
+                    return min(a_right, b_right)
+                return (max(a_left, b_left) + min(a_right, b_right)) / 2
+            elif a_left > b_right: 
+                r = ai - 1
+            else:
+                l = ai + 1
+
+        
+
+
+        
+            
+                
+
+                
+
+                
+
+        
+
+
+
+
+        
+
+
